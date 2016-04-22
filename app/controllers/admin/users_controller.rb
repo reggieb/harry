a44ee3e99e69cbf8735eb395391a20a6a8b9c901
Rob Nichols <git@nicholshayes.co.uk>
2016-04-22 16:21:12 +0100

    Tidy up

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,28 +1,20 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /admin/users
-  # GET /admin/users.json
   def index
     @users = User.all
   end
 
-  # GET /admin/users/1
-  # GET /admin/users/1.json
   def show
   end
 
-  # GET /admin/users/new
   def new
     @user = User.new
   end
 
-  # GET /admin/users/1/edit
   def edit
   end
 
-  # POST /admin/users
-  # POST /admin/users.json
   def create
     @user = User.new(user_params)
 
@@ -35,8 +27,6 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /admin/users/1
-  # PATCH/PUT /admin/users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -47,8 +37,6 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  # DELETE /admin/users/1
-  # DELETE /admin/users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  root to: "flood_risk_engine/enrollments#new"
 
   devise_for :users
-  mount FloodRiskEngine::Engine => '/fre'
   
-  namespace :admin do
-    resources :users
+  authenticate :user do
+    mount FloodRiskEngine::Engine => '/fre'
+
+    namespace :admin do
+      resources :users
+    end
   end
   
 end
